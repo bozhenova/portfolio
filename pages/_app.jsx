bb882c356dfb59d88a2018469c4a24e8a0c49ed5
Margo <margo.bozhenova@gmail.com>
2021-01-05 13:30:57 +0300

    Add Footer and custom server

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,25 +2,23 @@ import App from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '@/components/shared/Navbar';
 import Header from '@/components/shared/Header';
+import Footer from '../components/shared/Footer';
 import '@/styles/index.scss';
 
 const MyApp = ({ Component, pageProps }) => {
+  const isHomePage = () => Component.name === 'Home';
+
   return (
     <div className='portfolio-app'>
       <Navbar />
       {pageProps.appData}
-      {Component.name === 'Home' && <Header />}
+      {isHomePage() && <Header />}
       <div className='container'>
         <Component {...pageProps} />
       </div>
+      {isHomePage() && <Footer />}
     </div>
   );
-};
-
-MyApp.getInitialProps = async (context) => {
-  const initialProps =
-    App.getInitialProps && (await App.getInitialProps(context));
-  return { pageProps: { ...initialProps.pageProps, appData: 'Some app data' } };
 };
 
 export default MyApp;
