fe913fe0ce85865eca8eb1c23af152caeb3190fe
Margo <margo.bozhenova@gmail.com>
2021-01-09 14:37:52 +0300

    Add resolvers

--- server/index.js
+++ server/index.js
@@ -8,17 +8,62 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const data = {
+  portfolios: [
+    {
+      _id: 'rf3f3f3',
+      title: 'Job in USA',
+      content: 'It was very nice experience',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false,
+    },
+    {
+      _id: 'rf3fefef3f3',
+      title: 'Job in Barcelona',
+      content: 'It was very sunny',
+      jobTitle: 'Developer',
+      isCurrentlyEmployed: true,
+    },
+    {
+      _id: 'rf3f3k0k0f3',
+      title: 'Job in London',
+      content: 'It was very rainy',
+      jobTitle: 'Frontend-developer',
+      daysOfExperience: 365,
+      isCurrentlyEmployed: false,
+    },
+  ],
+};
+
 app.prepare().then(() => {
   const server = express();
 
   const schema = buildSchema(`
-  type Query {
-    hello: String
+    type Portfolio {
+      _id: ID
+      title: String
+      content: String!
+      jobTitle: String
+      daysOfExperience: Int
+      isCurrentlyEmployed: Boolean
+  }
+
+    type Query {
+      hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
   }
   `);
 
   const root = {
     hello: () => 'Hello',
+    portfolio: () => {
+      return data.portfolios[0];
+    },
+    portfolios: () => {
+      return data.portfolios;
+    },
   };
 
   server.use(
